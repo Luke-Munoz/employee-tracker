4b1dfa5796084a39bfa8ed48cc35120ee69e27cb
Luke <munozluke1@gmail.com>
2021-10-14 21:56:11 -0600

    what is stopping it from seeding

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,16 @@
-USE employee;
-
 INSERT INTO department(id, name_of_department)
 VALUES
-(1, sales),
-(2, finance),
-(3, legal),
-(4, engineering);
+(1, 'sales'),
+(2, 'finance'),
+(3, 'legal'),
+(4, 'engineering');
 
 INSERT INTO roles (id, salary, department_id, name)
 VALUES 
 (1, 4000, 2, 'sales manager'),
 (2, 2000, 3, 'lawyer'),
 (3, 1500, 4, 'Head engineer'),
-(4, 5000, 1 'Finacial advisor');
+(4, 5000, 1 'Finacial advisor'),;
 
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
